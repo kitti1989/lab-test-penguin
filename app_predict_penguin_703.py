6295b54f4bfa521a9dd11f36a31cd384f4b563aa
kitti1989 <kittipat.puan@gmail.com>
2024-11-10 13:04:20 +0700

    Update app_predict_penguin_703.py

--- app_predict_penguin_703.py
+++ app_predict_penguin_703.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 import pickle
 import numpy as np
+import pandas as pd 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import LabelEncoder
